223df5d8adbf494c0640346b669c007b35116d97
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-04-16 21:07:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+function fetchBooks() { 
+    return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((data) => renderBooks(data));
 }
 
 function renderBooks(books) {
@@ -13,5 +14,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
 });
